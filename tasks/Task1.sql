7a6ee756de41c83dd487d2b59af70bf2692bbcb2
hpk92 <40123595+hpk92@users.noreply.github.com>
2018-08-26 22:32:49 +0200

    Update Task1.sql

--- tasks/Task1.sql
+++ tasks/Task1.sql
@@ -1,7 +1,8 @@
 /* Total sum of products per week */
 
-SELECT count(product_id) as TotalSold FROM (
-							select product_id, week(Purchase_date) as dates 
-                            from foodspring.Sales
-                            ) sub
+SELECT count(product_id) as TotalSold 
+FROM (
+	select product_id, week(Purchase_date) as dates 
+        from foodspring.Sales
+      ) sub
 where sub.dates = 34 /* Week number */
