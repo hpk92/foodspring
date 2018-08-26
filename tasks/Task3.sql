ef493ccf98412e2177e8aa24a6449efeffc8ccfd
hpk92 <40123595+hpk92@users.noreply.github.com>
2018-08-26 22:34:36 +0200

    Update Task3.sql

--- tasks/Task3.sql
+++ tasks/Task3.sql
@@ -11,21 +11,23 @@ sum(result.pb) as 'Protein Bar'
 from
 (
     SELECT
-	pivot.Client_Name as clientName,
+    pivot.Client_Name as clientName,
     CASE WHEN pivot.product_name='Whey Protein' THEN pivot.amount ELSE 0 END as wp,
     CASE WHEN pivot.product_name='Shape Shake' THEN pivot.amount ELSE 0 END as ss,
     CASE WHEN pivot.product_name='Goji Beeren' THEN pivot.amount ELSE 0 END as gb,
     CASE WHEN pivot.product_name='Protein Bar' THEN pivot.amount ELSE 0 END as pb
     from
         (
-        SELECT c.Client_Name as Client_Name ,p.product as product_name, pur.amount as amount FROM foodspring.product as p, foodspring.client as c,
-            (
-            SELECT sum(purc.amount) as amount,purc.ClientId as ClientId, purc.ProductId as ProductId FROM foodspring.purchase as purc
+        SELECT c.Client_Name as Client_Name ,p.product as product_name, pur.amount as amount 
+	FROM foodspring.product as p, foodspring.client as c,
+        (
+            SELECT sum(purc.amount) as amount,purc.ClientId as ClientId, purc.ProductId as ProductId 
+            FROM foodspring.purchase as purc
             group by ProductId, ClientId
-            ) pur
-            where
-                pur.ProductId = p.ID  and pur.ClientId =c.ID 
-            order by Client_Name
+        ) pur
+        where
+          pur.ProductId = p.ID  and pur.ClientId =c.ID 
+        order by Client_Name
         ) pivot
 ) result
 group by clientName
