b6a44f0a4c2e973f28da41d887037d5604182f01
hpk92 <40123595+hpk92@users.noreply.github.com>
2018-08-27 10:57:22 +0200

    Update create_product.sql

--- DB_Creation/create_product.sql
+++ DB_Creation/create_product.sql
@@ -1,14 +1,14 @@
 CREATE TABLE `foodspring`.`product` (
   `id` INT NOT NULL,
-  `product_name` VARCHAR(45) NOT NULL,
+  `product` VARCHAR(45) NOT NULL,
   PRIMARY KEY (`id`));
 
 
 
-INSERT INTO foodspring.product (ID,ProductName)
+INSERT INTO foodspring.product (id,product)
 VALUES 
-(1, 'Whey Protein'),
-(2,'Shape Shake '),
+(1,'Whey Protein'),
+(2,'Shape Shake'),
 (3,'Goji Beeren'),
 (6,'Protein Bar')
 ;
